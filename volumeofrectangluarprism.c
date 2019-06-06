f881ab272e395546fe845b4bd33ed75f1831eacd
kurpanekt7074 <50212236+kurpanekt7074@users.noreply.github.com>
2019-06-06 10:31:50 -0700

    Create volumeofrectangluarprism.c

---
+++ volumeofrectangluarprism.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+ 
+int main() {
+   float length, width, height, area;
+ 
+   printf("\nEnter the Length of Rectangle : ");
+   scanf("%f", &length);
+ 
+   printf("\nEnter the Width of Rectangle : ");
+   scanf("%f", &width);
+
+   printf("\nEnter the Width of Rectangle : ");
+   scanf("%f", &height);
+ 
+ 
+   area = length * width * height;
+   printf("\nVolume of Rectangluarprism : %f\n", area);
+ 
+   return (0);
+}
+
+
+
+
+
+
+
