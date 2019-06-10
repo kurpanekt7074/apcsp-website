6791c404dc052f420336fba1d4971cbf0212f7cd
kurpanekt7074 <50212236+kurpanekt7074@users.noreply.github.com>
2019-06-10 11:02:42 -0700

    Update volumeofpyramid.c

--- volumeofpyramid.c
+++ volumeofpyramid.c
@@ -1,20 +1,27 @@
-
 #include<stdio.h>
-
+#include<math.h>
  
 int main() {
    float length, width, height, area;
  
+   for (int j = 1; j <= 3; j++)
+   {
+    if (j==1)
+    {
    printf("\nEnter the Length of Pyramid : ");
    scanf("%f", &length);
- 
+    }
+    if (j==2)
+    {
    printf("\nEnter the Width of Pyramid : ");
    scanf("%f", &width);
-
-   printf("\nEnter the height of Pyramid : ");
+    }
+    if (j==3){
+   printf("\nEnter the Height of Pyramid : ");
    scanf("%f", &height);
- 
- 
+   }
+   }
+     
    area = length * width * height / 3;
    printf("\nVolume of Pyramid : %f\n", area);
  
