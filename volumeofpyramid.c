98dda5153b9810153451aeb1235e998daeed677a
kurpanekt7074 <50212236+kurpanekt7074@users.noreply.github.com>
2019-06-10 22:32:47 -0700

    Update volumeofpyramid.c

--- volumeofpyramid.c
+++ volumeofpyramid.c
@@ -1,42 +1,52 @@
 
 #include<stdio.h>
-#include<math.h>
- 
+#include <math.h>
 int main() {
-   float length, width, height, area;
- 
-   for (int j = 1; j <= 3; j++)
+   float length, width, height, volume;
+   for (int j = 1; j <= 5; j++)
    {
-    if (j==1)
-    {
+     if (j==1)
+   {
    printf("\nEnter the Length of Pyramid : ");
    scanf("%f", &length);
-    }
-    if (j==2)
-    {
+   if (length < 0)
+   {
+     printf("please use a positive number");
+      j--;
+   }
+   }
+   else if (j==2)
+   {
    printf("\nEnter the Width of Pyramid : ");
    scanf("%f", &width);
-    }
-    if (j==3){
+   if (width < 0)
+   {
+     printf("please use a positive number");
+      j--;
+   }
+   }
+   else if (j==3)
+   {
    printf("\nEnter the Height of Pyramid : ");
    scanf("%f", &height);
+   if (height < 0)
+   {
+     printf("please use a positive number");
+      j--;
    }
    }
-
-    for (int i = 1; i <=2; i++)
-    {
-    
-    if (i==1)
-    {
-      area = length * width * height / 3;
-    }
-   
-    if (i==2) 
-    {
-   printf("\nVolume of Pyramid : %f\n", area);
-    }
-    }
+   else if (j==4)
+   {
+   volume = length * width * height /3;
+   printf("\nVolume of Pyramid : %f\n", volume);
+   }
+   else if (j==5)
+   {
    return (0);
+   }
+   }
 }
+
+
 
 
