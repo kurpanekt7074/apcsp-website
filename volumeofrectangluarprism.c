7d816dbaca8eeea5a3254f441404ba38c62d5b8a
kurpanekt7074 <50212236+kurpanekt7074@users.noreply.github.com>
2019-06-11 09:59:21 -0700

    Update volumeofrectangluarprism.c

--- volumeofrectangluarprism.c
+++ volumeofrectangluarprism.c
@@ -1,70 +1,61 @@
-
-
 #include<stdio.h>
 #include <math.h>
 int main() {
+ 
 
-FILE*outFile;
-   outFile = fopen("outputfinal", "w");
- if (outFile == NULL) {
+FILE*outFiles;
+   outFiles = fopen("outputPrism", "w");
+ if (outFiles == NULL) {
    printf("error - could not open file\n");
  return 1;
 }
 
-   float length, width, height, volume;
+
+
+  float length, width, height, area;
    for (int j = 1; j <= 5; j++)
    {
      if (j==1)
    {
-   printf("\nEnter the Length of Pyramid : ");
-
+   printf("\nEnter the Length of Rectangular Prism : ");
    scanf("%f", &length);
-
    if (length < 0)
    {
-     printf("please use a positive number");
+     printf("please use a postive number");
       j--;
    }
-    }
+   }
    else if (j==2)
    {
-   printf("\nEnter the Width of Pyramid : ");
+   printf("\nEnter the Width of Rectangular Prism : ");
    scanf("%f", &width);
-   if (width < 0)
+        if (width < 0)
    {
-     printf("please use a positive number");
+     printf("please use a postive number");
       j--;
    }
    }
    else if (j==3)
    {
-   printf("\nEnter the Height of Pyramid : ");
+   printf("\nEnter the Height of Rectangular Prism : ");
    scanf("%f", &height);
    if (height < 0)
    {
-     printf("please use a positive number");
-      j--;   
-     }
+     printf("please use a postive number");
+      j--;
    }
-   else if (j==4)
+   }
+      else if (j==4)
    {
-   volume = length * width * height /3;
-   printf("\nVolume of Pyramid : %f\n", volume);
-fprintf(outFile, "Volume of Pyramid :%f\n", volume);
+   area = length * width * height;
+   printf("\nVolume of Rectangluarprism : %f\n", area);
+    fprintf(outFiles, "Volume of Rectangular Prism :%f\n", area);   
    }
+
+
    else if (j==5)
    {
    return (0);
    }
    }
 }
-
-
-
-    
-    
-    
-    
-    
-    
-    
