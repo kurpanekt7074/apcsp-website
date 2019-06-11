467bc382068c1a687bbdbee4d4acb8a67aba45bd
kurpanekt7074 <50212236+kurpanekt7074@users.noreply.github.com>
2019-06-11 09:55:24 -0700

    Update volumeofpyramid.c

--- volumeofpyramid.c
+++ volumeofpyramid.c
@@ -2,19 +2,29 @@
 #include<stdio.h>
 #include <math.h>
 int main() {
+
+FILE*outFile;
+   outFile = fopen("outputfinal", "w");
+ if (outFile == NULL) {
+   printf("error - could not open file\n");
+ return 1;
+}
+
    float length, width, height, volume;
    for (int j = 1; j <= 5; j++)
    {
      if (j==1)
    {
    printf("\nEnter the Length of Pyramid : ");
+
    scanf("%f", &length);
+
    if (length < 0)
    {
      printf("please use a positive number");
       j--;
    }
-   }
+    }
    else if (j==2)
    {
    printf("\nEnter the Width of Pyramid : ");
@@ -32,13 +42,14 @@ int main() {
    if (height < 0)
    {
      printf("please use a positive number");
-      j--;
-   }
+      j--;   
+     }
    }
    else if (j==4)
    {
    volume = length * width * height /3;
    printf("\nVolume of Pyramid : %f\n", volume);
+fprintf(outFile, "Volume of Pyramid :%f\n", volume);
    }
    else if (j==5)
    {
@@ -46,7 +57,4 @@ int main() {
    }
    }
 }
-
-
-
 
