c1c915bc752be12ae79f599627b3c3259bb07059
kurpanekt7074 <50212236+kurpanekt7074@users.noreply.github.com>
2019-06-11 09:53:06 -0700

    Update volumeofrectangluarprism.c

--- volumeofrectangluarprism.c
+++ volumeofrectangluarprism.c
@@ -1,28 +1,70 @@
+
+
 #include<stdio.h>
-
- 
+#include <math.h>
 int main() {
-   float length, width, height, area;
- 
-   printf("\nEnter the Length of Rectangle : ");
-   scanf("%f", &length);
- 
-   printf("\nEnter the Width of Rectangle : ");
-   scanf("%f", &width);
 
-   printf("\nEnter the Width of Rectangle : ");
+FILE*outFile;
+   outFile = fopen("outputfinal", "w");
+ if (outFile == NULL) {
+   printf("error - could not open file\n");
+ return 1;
+}
+
+   float length, width, height, volume;
+   for (int j = 1; j <= 5; j++)
+   {
+     if (j==1)
+   {
+   printf("\nEnter the Length of Pyramid : ");
+
+   scanf("%f", &length);
+
+   if (length < 0)
+   {
+     printf("please use a positive number");
+      j--;
+   }
+    }
+   else if (j==2)
+   {
+   printf("\nEnter the Width of Pyramid : ");
+   scanf("%f", &width);
+   if (width < 0)
+   {
+     printf("please use a positive number");
+      j--;
+   }
+   }
+   else if (j==3)
+   {
+   printf("\nEnter the Height of Pyramid : ");
    scanf("%f", &height);
- 
- 
-   area = length * width * height;
-   printf("\nVolume of Rectangluarprism : %f\n", area);
- 
+   if (height < 0)
+   {
+     printf("please use a positive number");
+      j--;   
+     }
+   }
+   else if (j==4)
+   {
+   volume = length * width * height /3;
+   printf("\nVolume of Pyramid : %f\n", volume);
+fprintf(outFile, "Volume of Pyramid :%f\n", volume);
+   }
+   else if (j==5)
+   {
    return (0);
+   }
+   }
 }
 
 
 
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
